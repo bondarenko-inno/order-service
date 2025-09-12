e681fc9af7e5d8fd935fdc0fb9a21f3ae4628c06
bondarenko-inno <yahor.bandarenka@innowise.com>
2025-09-12 11:38:42 +0300

    item functionality

---
+++ src/main/java/org/ebndrnk/orderservice/model/dto/ItemResponse.java
@@ -0,0 +1,6 @@
+package org.ebndrnk.orderservice.model.dto;
+
+public record ItemResponse (Long id,
+                            String name,
+                            Double price) {
+}
--- src/main/java/org/ebndrnk/orderservice/service/ItemService.java
+++ src/main/java/org/ebndrnk/orderservice/service/ItemService.java
@@ -3,15 +3,22 @@ package org.ebndrnk.orderservice.service;
 import lombok.RequiredArgsConstructor;
 import org.ebndrnk.orderservice.exception.ItemNotFoundException;
 import org.ebndrnk.orderservice.exception.NotEnoughStock;
+import org.ebndrnk.orderservice.mapper.ItemMapper;
+import org.ebndrnk.orderservice.model.dto.ItemDto;
+import org.ebndrnk.orderservice.model.dto.ItemResponse;
 import org.ebndrnk.orderservice.model.entity.Item;
 import org.ebndrnk.orderservice.repository.ItemRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+    private final ItemMapper itemMapper;
 
 
     public Item getItemById(Long id) {
@@ -38,4 +45,14 @@ public class ItemService {
     }
 
 
+    public List<ItemResponse> getAll() {
+        return itemRepository.findAll().stream()
+                .map(itemMapper::entityToResponse)
+                .toList();
+    }
+
+    public ItemResponse getItemDtoById(Long id) {
+        return itemMapper.entityToResponse(itemRepository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException("Item not found: " + id)));
+    }
 }
